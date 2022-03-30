1c76dc5c7bf8bbffa98feae8c28a4c2d62c691b1
yasiralshawk <yasir.alshawk12@gmail.com>
2022-03-30 10:55:40 -0400

    wrote the code

--- src/day0/First.java
+++ src/day0/First.java
@@ -2,8 +2,9 @@ package day0;
 
 public class First {
 
-    //TODO: working on this tomorrow
-    // add some more actions
+    public static void main(String[] args) {
+        System.out.println("java");
+    }
 // extra line
     // hello
     // java
